f0f3ac2f3bcabb00b6623622c97655fa2c3cf4a5
sangyunpark99 <psangyun5108@gmail.com>
2026-03-03 09:51:38 +0900

    패키지 이동

---
+++ src/main/java/org/hello/backjoon/문제풀이/DP/게임개발/Main.java
@@ -0,0 +1,63 @@
+package org.hello.backjoon.문제풀이.DP.게임개발;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static int N;
+    private static int[] time;
+    private static List<List<Integer>> build = new ArrayList<>(); // 해당 idx가 선행인 모든 건물들
+    private static int[] buildCnt;
+    private static int[] cost;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        time = new int[N + 1];
+        buildCnt = new int[N + 1];
+        cost = new int[N + 1];
+
+        for(int i = 0; i <= N; i++) {
+            build.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < N; i++) {
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int cost = arr[0];
+            int number = i + 1;
+            time[number] = cost;
+            int[] bfBuild = Arrays.copyOfRange(arr, 1, arr.length - 1); // 이전에 지어야할 건물
+            buildCnt[number] = bfBuild.length;
+            for(int j = 0; j < bfBuild.length; j++) {
+                build.get(bfBuild[j]).add(number);
+            }
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>(); // 지어진 건물
+
+        for(int i = 1; i <= N; i++) {
+            if(buildCnt[i] == 0) {
+                cost[i] = time[i];
+                queue.offer(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+            for(int next : build.get(cur)) {
+                cost[next] = Math.max(cost[next], cost[cur] + time[next]); // 매번 비용 갱신
+                if(--buildCnt[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        for(int i = 1; i <= N; i++) {
+            System.out.println(cost[i]);
+        }
+    }
+}
---
+++ src/main/java/org/hello/backjoon/문제풀이/큐/트럭/Main.java
@@ -0,0 +1,45 @@
+package org.hello.backjoon.문제풀이.큐.트럭;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static int N;
+    private static int W;
+    private static int L;
+    private static int[] trucks;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        trucks = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Queue<Integer> bridge = new ArrayDeque<>(W);
+        for(int i = 0; i < W; i++) bridge.offer(0);
+        int idx = 0;
+        int time = 0;
+        int weight = 0;
+        while(idx < N) {
+            time++;
+            weight -= bridge.poll();
+            int next = trucks[idx];
+            if(weight + next <= L) {
+                bridge.offer(next);
+                weight += next;
+                idx++;
+            }else {
+                bridge.offer(0);
+            }
+        }
+
+        System.out.println(time + W); // 마지막에 트럭이 지나가는 경우
+    }
+}
